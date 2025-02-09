b68b3e02a3080f5b16dc6f46113b7b818e1708fb
avalen2022 <a.valencia.2022@alumnos.urjc.es>
2025-02-09 23:03:44 +0100

    Corrected forces

--- optional_1/review.py
+++ optional_1/review.py
@@ -24,15 +24,16 @@ for j in range (numJoints):
     print("%d - %s" % (p.getJointInfo(robotId, j)[0], p.getJointInfo(robotId, j)[1].decode("utf-8")))
 
 #rotateId = p.addUserDebugParameter("rotate_vel", -30, 30, 5)
-frictionId = p.addUserDebugParameter("friction", 0, 3, 0.03)
-torqueId = p.addUserDebugParameter("torque", -20, 20, -0)
+frictionId = p.addUserDebugParameter("friction", 0, 5, 0.05)
+torqueId = p.addUserDebugParameter("torque", -20, 20, 0)
 
 while(1):
     #vel_fore = p.readUserDebugParameter(rotateId)
     friction_fore = p.readUserDebugParameter(frictionId)
     torque_fore = p.readUserDebugParameter(torqueId)
     p.setJointMotorControl2(robotId, 1, p.TORQUE_CONTROL, force=torque_fore)
-    p.changeDynamics(robotId, 1, lateralFriction=friction_fore)
+    p.setJointMotorControl2(robotId, 1, p.VELOCITY_CONTROL, targetVelocity=0, force=friction_fore)
+    p.changeDynamics(robotId, 1, spinningFriction=friction_fore)
 
     p.stepSimulation()
     time.sleep(1/240.)
